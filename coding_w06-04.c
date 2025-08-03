e4ce437da01810aee985974d316ec439b454e195
surayuthpanngom <s6812022611341@email.kmutnb.ac.th>
2025-08-04 00:53:21 +0700

    Delete coding_w06-04.c

--- coding_w06-04.c
+++
@@ -1,9 +0,0 @@
-// แบบเต็ม -> แบบย่อ
-
-x = x - 4.0;                           // x -= 4.0;
-x = 6.5 * x;                           // x *= 6.5;
-x = x % (y + z * a);                   // x %= (y + z * a);
-x = x / (2.0 * x);                     // x /= (2.0 * x);
-total = total + (price * quantity - discount);  // total += (price * quantity - discount);
-x = x * (1 + rate / 100);             // x *= (1 + rate / 100);
-score = score - (penalty * (mistake + 1));  // score -= (penalty * (mistake + 1));
